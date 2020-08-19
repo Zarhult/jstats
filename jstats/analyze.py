ee4c847b87f1fc59214d30c0099557d046c4a7d4
Calvin Glisson <cglis001@ucr.edu>
2020-08-18 20:16:32 -0700

    Basic support for importing user knowledge (no storage)

--- jstats/analyze.py
+++ jstats/analyze.py
@@ -8,24 +8,34 @@ import requests
 from bs4 import BeautifulSoup
 from natto import MeCab
 
-
 Analytics = namedtuple('Analytics', [
     'freq_list',
     'cutoff_dict',
     'total_morphs',
-    'unique_morphs',
+    'total_known_morphs',
+    'unique_unknown_morphs'
 ])
 Analytics.__doc__ = '''\
     The Analytics object contains all the data gathered and organized
     from whatever source.
 
     Attributes:
-        freq_list: sorted list of tuples of morpheme/its frequency
+        freq_list: sorted list of tuples of unknown morpheme/its frequency
         cutoff_dict: a dictionary mapping desired percent comprehension
                      to necessary number of (most common) unique
-                     morphemes known
+                     unknown morphemes learned
         total_morphs: total number of morphemes, including duplicates
-        unique_morphs: total number of unique morphemes'''
+        total_known_morphs: total number of known morphemes, including
+                            duplicates
+        unique_unknown_morphs: total number of unique unknown morphemes'''
+
+# Make MeCab output nothing but morphemes
+fmt = {'node_format': r'%m\n',
+       'bos_format': r'',
+       'eos_format': r'',
+       'unk_format': r''}
+
+mecab = MeCab(options=fmt)
 
 
 def is_cjk(char):
@@ -56,51 +66,87 @@ def get_soup(html):
     return soup
 
 
-def generate_analytics(strings):
-    '''Returns analytics given list of strings.'''
-    # Make MeCab output nothing but morphemes
-    fmt = {'node_format': r'%m\n',
-           'bos_format': r'',
-           'eos_format': r'',
-           'unk_format': r''}
-
-    mecab = MeCab(options=fmt)
-
-    # First generate frequency list and count morphemes.
-    print('Generating frequency list...')
-    freq_dict = dict()
-    total_morphs = 0
+def get_morpheme_list(strings):
+    '''Returns list of morpheme strings found in list of strings.'''
+    morpheme_list = []
     for line in strings:
-        # Ignore non-UTF-8 characters
+        # Ignore non-UTF-8 characters/strings
         try:
             tokens = mecab.parse(line)
             tokens = tokens.splitlines()
+            morpheme_list += tokens
         except UnicodeDecodeError:
             pass
-        for string in tokens:
-            # Exclude unhelpful single-non-CJK-character morphemes
-            if len(string) > 1 or len(string) == 1 and is_cjk(string):
+
+    return morpheme_list
+
+
+def generate_analytics(strings, knowfile=''):
+    '''Returns analytics given list of strings to analyze and optionally the
+    path to a file containing known morphemes to factor in to the analysis.
+    Known morphemes will be left out of the frequency list, and used to tell
+    the user their current comprehension and how many unknown morphemes must
+    be learned to reach the comprehension cutoffs.
+    '''
+    # TODO: this function is disgusting
+
+    # Get known morphemes if provided, otherwise none known
+    known_morphemes = []
+    if knowfile:
+        print('Parsing known morphemes...')
+        f = open(knowfile, 'r', encoding='utf-8')
+        known_morphemes = get_morpheme_list(f.readlines())
+        # Remove duplicates
+        known_morphemes = list(dict.fromkeys(known_morphemes))
+
+    # First generate frequency list and count morphemes.
+    print('Generating frequency list...')
+    input_morphemes = get_morpheme_list(strings)
+    freq_dict = dict()
+    total_morphs = 0
+    total_known_morphs = 0
+
+    for string in input_morphemes:
+        # Exclude unhelpful single-non-CJK-character morphemes and known
+        # morphemes in the frequency list, and count total morphemes and total
+        # known morphemes ('total' as in including repeats)
+        not_known = string not in known_morphemes
+        valid = len(string) > 1 or len(string) == 1 and is_cjk(string)
+        if valid:
+            total_morphs += 1
+            if not_known:
                 if string in freq_dict:
                     freq_dict[string] += 1
                 else:
                     freq_dict[string] = 1
-                total_morphs += 1
+            else:
+                total_known_morphs += 1
 
     freq_list = sorted(freq_dict.items(), key=lambda x: x[1], reverse=True)
 
-    # Now generate cutoff dictionary using frequency list info
+    # Now generate cutoff dictionary using frequency list/morpheme count info
+    # using accumulator of how many total morphemes in the text are covered,
+    # first by known morphemes and then by factoring in the most frequent
+    # unknown morphemes
     print('Determining comprehension cutoffs...')
-    cutoff_dict = {80: 0, 85: 0, 90: 0, 95: 0, 98: 0, 99: 0}
-    accumulator = 0
+    # -1 = unset
+    cutoff_dict = {80: -1, 85: -1, 90: -1, 95: -1, 98: -1, 99: -1}
+    accumulator = total_known_morphs
+    # Must first check if known morphemes already reach cutoff(s)
+    for key in cutoff_dict:
+        if key <= accumulator/total_morphs * 100:
+            cutoff_dict[key] = 0
+    # Now set the cutoffs that require more knowledge
     for i, morph in enumerate(freq_list, start=1):
         accumulator += morph[1]
-        # !Should optimize this to only check lowest unset dict entry each loop
+        # !TODO: optimize this to only check lowest unset dict entry each loop
         for key in cutoff_dict:
-            if (cutoff_dict[key] == 0 and
+            if (cutoff_dict[key] == -1 and
                     key <= accumulator/total_morphs * 100):
                 cutoff_dict[key] = i
 
-    # Finally get number of unique morphemes
-    unique_morphs = len(freq_dict)
+    # Finally get number of unique unknown morphemes
+    unique_unknown_morphs = len(freq_dict)
 
-    return Analytics(freq_list, cutoff_dict, total_morphs, unique_morphs)
+    return Analytics(freq_list, cutoff_dict, total_morphs, total_known_morphs,
+                     unique_unknown_morphs)
--- jstats/jstats.py
+++ jstats/jstats.py
@@ -6,6 +6,7 @@ Planned features:
 - Option to print to stdout
 - Allow importing user's known morphemes from an input file, rather than
 analyzing file (first from CLI without any permanent storage)
+- Replace y/n prompts with "empty for none"
 '''
 
 import os.path
@@ -60,11 +61,14 @@ def get_parser():
     group.add_argument('-u', '--url',
                        help='specify a web URL for analysis')
     group.add_argument('-i', '--infile',
-                       help='specify an input file for analysis \
-                       (.html or .txt)')
+                       help=('specify an input file for analysis'
+                             ' (.html or .txt)'))
     parser.add_argument('-o', '--outfile',
                         help='specify output file (defaults to stats.txt)',
                         default='stats.txt')
+    parser.add_argument('-k', '--knowfile',
+                        help=('specify an input file containing'
+                              ' morphemes you know (.html or .txt)'))
 
     return parser
 
@@ -81,9 +85,13 @@ def interactive_set_args(args):
     elif i == 'f':
         args.infile = noblank_prompt('Enter filename: ')
 
+    i = options_prompt('Input a knowledge file? (y/n): ', ['y', 'n'])
+    if i == 'y':
+        args.knowfile = noblank_prompt('Enter filename: ')
+
 
 def get_url_analytics(args):
-    '''Return analytics given args specifying a URL.'''
+    '''Returns analytics given args specifying a URL.'''
     # If invalid url, see if prepending http:// works; otherwise quit
     if not valid_url(args.url):
         args.url = 'http://' + args.url
@@ -93,17 +101,18 @@ def get_url_analytics(args):
 
     html = analyze.load_page(args.url)
     soup = analyze.get_soup(html)
-    return analyze.generate_analytics(soup.stripped_strings)
+
+    return analyze.generate_analytics(soup.stripped_strings, args.knowfile)
 
 
 def get_infile_analytics(args):
-    '''Return analytics given args specifying an infile.'''
+    '''Returns analytics given args specifying an infile.'''
     infile_extension = os.path.splitext(args.infile)[1]
 
     if infile_extension in {'.html', '.htm'}:
         html = open(args.infile, 'r', encoding='utf-8')
         soup = analyze.get_soup(html)
-        return analyze.generate_analytics(soup.stripped_strings)
+        return analyze.generate_analytics(soup.stripped_strings, args.knowfile)
 
     if infile_extension in {'.txt', ''}:
         if infile_extension == '':
@@ -115,7 +124,7 @@ def get_infile_analytics(args):
 
         txt = open(args.infile, 'r', encoding='utf-8')
 
-        return analyze.generate_analytics(txt.readlines())
+        return analyze.generate_analytics(txt.readlines(), args.knowfile)
 
     print('Invalid file extension.')
     sys.exit(1)
@@ -126,29 +135,50 @@ def output_analytics(analytics, file_name=''):
     If the provided analytics object has no morphemes, instead just prints
     that no morphemes were found.
     '''
+    # ! TODO: optimize by moving the if statements outside of the for loops
     if analytics.total_morphs > 0:
-        if file_name != '':
+        using_output_file = file_name != ''
+        if using_output_file:
             print('Writing results to ' + file_name + '...')
-            file = open(file_name, 'w', encoding='utf-8')
+            f = open(file_name, 'w', encoding='utf-8')
+
+        comprehension = analytics.total_known_morphs / analytics.total_morphs
+        # 0 comprehension means user hasn't input a knowledge file
+        if comprehension != 0:
+            comprehension_str = str(round(comprehension * 100))
+            output_line1 = ('Your current comprehension is '
+                            + comprehension_str + '%.')
+            output_line2 = ('(You know ' + str(analytics.total_known_morphs)
+                            + ' of ' + str(analytics.total_morphs)
+                            + ' morphemes.)')
+
+            if using_output_file:
+                f.write(output_line1 + '\n')
+                f.write(output_line2 + '\n')
+            else:
+                print(output_line1)
+                print(output_line2)
 
         for key in analytics.cutoff_dict:
-            output_line = (str(key) + '% comprehension is at unique morpheme '
+            output_line = (str(key)
+                           + ('% comprehension is gained after unknown unique '
+                           'morpheme ')
                            + str(analytics.cutoff_dict[key]) + ' of '
-                           + str(analytics.unique_morphs))
-            if file_name != '':
-                file.write(output_line + '\n')
+                           + str(analytics.unique_unknown_morphs))
+            if using_output_file:
+                f.write(output_line + '\n')
             else:
                 print(output_line)
 
         for i, morph in enumerate(analytics.freq_list, start=1):
             output_line = (str(i) + ': ' + str(morph[0]) + ' ' + str(morph[1]))
-            if file_name != '':
-                file.write(output_line + '\n')
+            if using_output_file:
+                f.write(output_line + '\n')
             else:
                 print(output_line)
 
-        if file_name != '':
-            file.close()
+        if using_output_file:
+            f.close()
     else:
         print('No valid Japanese morphemes found.')
 
@@ -175,6 +205,8 @@ def main():
         analytics = get_url_analytics(args)
     elif args.infile:
         analytics = get_infile_analytics(args)
+    else:
+        raise Exception('No arguments set for analysis')
 
     output_analytics(analytics, args.outfile)
 
@@ -185,10 +217,12 @@ def main():
 if __name__ == '__main__':
     '''
     Catch and display all top-level exceptions, preventing crashes that
-    make output unreadable.
+    make output unreadable on Windows interactive mode.
     '''
     try:
         main()
+    except SystemExit:  # Don't print exit info when user runs -h
+        pass
     except BaseException:  # pylint: disable=broad-except
         print(sys.exc_info()[0])
         import traceback
